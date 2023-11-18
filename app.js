ef4236eeb27663ceccfbf663a11153159b29a17c
eapayson <eapayson@gmail.com>
2023-11-18 17:09:13 -0600

    fixed the rogue bingo row

--- app.js
+++ app.js
@@ -12,42 +12,23 @@ document.addEventListener('DOMContentLoaded', function () {
         shuffle(items);
         const cardContent = document.getElementById('card-content');
         cardContent.innerHTML = '';
-    
-        // Add the header row with BINGO letters
-        // const headerRow = document.createElement('div');
-        // headerRow.classList.add('bingo-row', 'header-row');
-        // letters.forEach(letter => {
-        //     const cell = document.createElement('div');
-        //     cell.classList.add('bingo-cell');
-        //     cell.textContent = letter;
-        //     headerRow.appendChild(cell);
-        // });
-        // cardContent.appendChild(headerRow);
-    
-        // Add the content rows
+
         for (let i = 0; i < 5; i++) {
             const row = document.createElement('div');
             row.classList.add('bingo-row');
-    
+
             for (let j = 0; j < 5; j++) {
                 const cell = document.createElement('div');
                 cell.classList.add('bingo-cell');
-                if (i === 0) {
-                    // Use BINGO letters for the first row
-                    cell.textContent = letters[j];
-                } else {
-                    // Use shuffled items for the content rows
-                    const index = (i - 1) * 5 + j;
-                    cell.textContent = items[index];
-                    cell.addEventListener('click', toggleColor);
-                }
+                const index = i * 5 + j - 5;
+                cell.textContent = items[index];
+                cell.addEventListener('click', toggleColor);
                 row.appendChild(cell);
             }
-    
+
             cardContent.appendChild(row);
         }
     }
-    
 
     function toggleColor() {
         this.classList.toggle('clicked');
