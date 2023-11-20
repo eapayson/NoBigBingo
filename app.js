6030873ccd4ce34de8cc473b447cb47d8541810d
eapayson <eapayson@gmail.com>
2023-11-20 12:58:22 -0600

    Update app.js
    added free space in the middle

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 const cell = document.createElement('div');
                 cell.classList.add('bingo-cell');
                 const index = i * 5 + j;
-                cell.textContent = items[index];
+				if (index == 12) {
+					cell.textContent = "FREE SPACE"
+				}
+				else {
+					cell.textContent = items[index];
+				}
                 cell.addEventListener('click', toggleColor);
                 row.appendChild(cell);
             }
