b24cfc0237fc995b5103989093686dd6674b5510
eapayson <eapayson@gmail.com>
2023-11-21 19:51:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const letters = ['B', 'I', 'N', 'G', 'O'];
-
+	const gridSize = 5;
+	
     function shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -26,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
 				}
 				else {
 					if (items[index].startsWith("Graphic ")) {
-						const remainingStr = items[index].substring("Graphic ".length);
+						const remainingStr = "assets/" + items[index].substring("Graphic ".length);
 						const imgElement = document.createElement('img');
 						imgElement.src = remainingStr;
 						cell.appendChild(imgElement);
@@ -35,16 +36,19 @@ document.addEventListener('DOMContentLoaded', function () {
 						cell.textContent = items[index];
 					}
 				}
-                cell.addEventListener('click', toggleColor);
                 row.appendChild(cell);
             }
 
             cardContent.appendChild(row);
         }
+		initCanvas()
     }
 
-    function toggleColor() {
-        this.classList.toggle('clicked');
+    function toggleCell(cell) {
+        cell.classList.toggle('clicked');
+		const allCells = document.querySelectorAll('.bingo-cell');
+		const cellIndex = Array.from(allCells).indexOf(cell);
+		console.log('Clicked cell index:', cellIndex);
     }
 
     // Load items from a text file
@@ -55,4 +59,86 @@ document.addEventListener('DOMContentLoaded', function () {
             generateBingoCard(items);
         })
         .catch(error => console.error('Error loading items:', error));
+	
+	const invisibleCanvas = document.getElementById('invisibleCanvas');
+	const invisibleContext = invisibleCanvas.getContext('2d');
+	
+	const canvas = document.getElementById('myCanvas');
+	const context = canvas.getContext('2d');
+	
+	const headerElement = document.querySelector('.header-row');
+	const headerHeight = headerElement.clientHeight;
+	
+	function initCanvas() {
+		const bingoCardElement = document.getElementById('bingo-card');
+		const totalHeight = bingoCardElement.clientHeight;
+		const totalWidth = bingoCardElement.clientWidth;
+		
+		canvas.setAttribute('width', totalWidth.toString());
+		canvas.setAttribute('height', totalHeight.toString());
+		invisibleCanvas.setAttribute('width', totalWidth.toString());
+		invisibleCanvas.setAttribute('height', totalHeight.toString());
+	}
+	function checkWinConditions() {
+		if(checkRows() || checkColumns() || checkDiagonals()) {
+			if(checkRows()) {
+				console.log('row win');
+			}
+			else if(checkColumns()) {
+				console.log('col win');
+			}
+			else if(checkDiagonals()){
+				console.log('diag win');
+			}
+			console.log('winner winner flavorless chicken dinner!');
+		}
+	}
+	function checkRows() {
+		const rows = document.querySelectorAll('.bingo-row');
+		return Array.from(rows).some(row => row.querySelectorAll('.clicked').length === gridSize);
+	}
+	function checkColumns() {
+        for (let j = 0; j < gridSize; j++) {
+            const column = document.querySelectorAll('.bingo-cell:nth-child(' + gridSize + 'n + ' + (j + 1) + ')');
+            if (Array.from(column).every(cell => cell.classList.contains('clicked'))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Function to check if either diagonal has been toggled
+    function checkDiagonals() {
+        const diagonal1 = document.querySelectorAll('.bingo-cell:nth-child(' + (gridSize + 1) + ')');
+        const diagonal2 = document.querySelectorAll('.bingo-cell:nth-child(' + (gridSize - 1) + 'n + ' + (gridSize - 1) + ')');
+
+		console.log(diagonal1);
+		console.log(diagonal2);
+
+        return (
+            Array.from(diagonal1).every(cell => cell.classList.contains('clicked')) ||
+            Array.from(diagonal2).every(cell => cell.classList.contains('clicked'))
+        );
+    }
+	
+	invisibleCanvas.addEventListener('click', handleInvisibleCanvasClick);
+	
+	function handleInvisibleCanvasClick(event) {
+        const rect = invisibleCanvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top - headerHeight;
+        // Implement logic to map (x, y) to the corresponding grid cell
+        // For simplicity, let's assume the grid is evenly spaced
+        const cellSize = canvas.width / gridSize;
+        const columnIndex = Math.floor(x / cellSize);
+        const rowIndex = Math.floor(y / cellSize);
+
+        // Toggle the corresponding cell on the visible grid
+        const cell = document.querySelector(`.bingo-row:nth-child(${rowIndex + 1}) .bingo-cell:nth-child(${columnIndex + 1})`);
+		console.log('calculated row/col: ', (rowIndex + 1),(columnIndex + 1))
+        if (cell) {
+            toggleCell(cell);
+            checkWinConditions();
+        }
+    }
 });
